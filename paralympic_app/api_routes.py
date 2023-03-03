d35c23358900b8dd8dde5d122522f476c01d05dc
Nicolas <zceinep@ucl.ac.uk>
2023-03-03 16:20:05 +0000

    ook

--- paralympic_app/api_routes.py
+++ paralympic_app/api_routes.py
@@ -1,5 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
-# Add the API routes to this file
+@bp.get('/noc')
+@bp.get('/noc/<code>')
+def noc_get(code=None):
+    """Returns a list of NOC codes with the country/region name and notes or if a code is passed then returns the details for that code """
+    if code:
+        return f"The details for {code}."
+    else:
+        return "Returns a list of NOC codes"
+
+@bp.post('/noc')
+@bp.patch('/noc/<code>')
+@bp.delete('/noc/<code>')
+def create_update_noc(code=None):
+    if request.method == "PATCH":
+        return f"The updated details for {code}"
+    elif request.method == "GET":
+        return "The record was successfully created.", 201
+    elif request.method == "DELETE":
+        return "The record was successfully deleted", 202
+
